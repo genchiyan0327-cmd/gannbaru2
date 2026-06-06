322bc3fd5400febc060c69a0ff4887eec72ecb2d
genchiyan0327-cmd <genchiyan0327@gmail.com>
2026-06-06 22:22:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-st.title("ロシア語単語帳")
-
 df = pd.read_csv("3言語2.csv")
 
-for _, row in df.iterrows():
-    with st.expander(row["Русский"]):
-        st.write(f"🇩🇪 {row['Deutsch']}")
-        st.write(f"🇺🇸 {row['English']}")
+st.write(df.columns.tolist())
